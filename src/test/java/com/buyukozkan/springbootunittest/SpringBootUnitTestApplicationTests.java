2143a23fb752bfee4214d5be0c8d2c077831dd39
halukbuyukozkan <halukbuyukozkan@gmail.com>
2024-07-28 14:45:12 +0300

    first commit

---
+++ src/test/java/com/buyukozkan/springbootunittest/SpringBootUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyukozkan.springbootunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
